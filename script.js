b4e330c0e4dc45ca712d2b96018f0d460621920e
shinjansengar <shinjansengar@gmail.com>
2022-03-09 16:10:58 +0530

    fixes

--- script.js
+++ script.js
@@ -8,18 +8,26 @@ var p2 = document.querySelector("#p2");
 var p3 = document.querySelector("#p3");
 var p4 = document.querySelector("#p4");
 
-idleBtn.addEventListener("click", (e)=>{  
+idleBtn.addEventListener("click", (e)=>{
   p1.style.display = "block";
   p2.style.display = "none";
   p3.style.display = "none";
   p4.style.display = "none";
+  p1.play();
+  p2.stop();
+  p3.stop();
+  p4.stop();
 });
 
-talkBtn.addEventListener("click", (e)=>{  
+talkBtn.addEventListener("click", (e)=>{
   p1.style.display = "none";
   p2.style.display = "block";
   p3.style.display = "none";
   p4.style.display = "none";
+  p2.play();
+  p1.stop();
+  p3.stop();
+  p4.stop();
 });
 
 confusedBtn.addEventListener("click", (e)=>{  
@@ -27,10 +35,18 @@ confusedBtn.addEventListener("click", (e)=>{
   p2.style.display = "none";
   p3.style.display = "block";
   p4.style.display = "none";
+  p3.play();
+  p1.stop();
+  p2.stop();
+  p4.stop();
 });
 excitedBtn.addEventListener("click", (e)=>{  
   p1.style.display = "none";
   p2.style.display = "none";
   p3.style.display = "none";
-  p4.style.display = "block";;
+  p4.style.display = "block";
+  p4.play();
+  p1.stop();
+  p2.stop();
+  p3.stop();
 });
